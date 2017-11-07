d08c25faf1e08d9cb3bece2989848c216a6fe3d3
opmarq <opmarq@gmail.com>
2017-11-07 16:31:46 +0000

    adding the upload to firebase feature but still needs improvements

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const downloader = require("./modules/downloader");
+const util = require('util');
+const request = require("request")
+const fs = require('fs');
 const cors = require('cors');
+const firebase = require('firebase');
+const googleStorage = require('@google-cloud/storage');
 const app = express();
 
 
@@ -13,6 +17,25 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 }));
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
+
+
+var config = {
+  apiKey: "AIzaSyCNfpWAOSSCacueDGfmoedXPdLaGC66Wo4",
+  authDomain: "wouldyou-9cb01.firebaseapp.com",
+  databaseURL: "https://wouldyou-9cb01.firebaseio.com",
+  projectId: "wouldyou-9cb01",
+  storageBucket: "wouldyou-9cb01.appspot.com",
+  messagingSenderId: "10270368950"
+};
+
+firebase.initializeApp(config);
+
+const storage = googleStorage({
+  projectId: "wouldyou-9cb01",
+  keyFilename: "./key.json"
+});
+
+const bucket = storage.bucket("wouldyou-9cb01.appspot.com");
 
 
 // if the user try to access server directly!
@@ -28,10 +51,104 @@ app.post('/backup', function (req, res) {
   let urls = JSON.parse(req.body.urls);
   let token = req.body.fbtoken;
 
+  const credential = firebase.auth.FacebookAuthProvider.credential(token);
+  
+  // Sign in with credential from the Google user.
+  firebase.auth().signInWithCredential(credential).catch((error)=> console.log(error) );
 
-  res.end("done");
+  firebase.auth().onAuthStateChanged((user)=>{
+    
+    if (user) {
+      
+      //console.log(user.displayName)
+
+      urls.forEach((url)=>{
+
+        let fileName = url.match(/\/(\w+)\//)[1];
+
+        requestImage(url).then((data)=>{
+
+          let file = {
+            buffer: data,
+            name: `${user.uid}/${ fileName }.jpg`,
+            mime: "image/png"
+          }
+        
+          uploadImageToStorage(file).then((data)=>{
+        
+            console.log(data);
+        
+          });
+
+        });
+
+      });
+
+    } else {
+
+      console.log("not signed in!!");
+    
+    }
+  });
+
 
 });
 
 app.listen(8000);
+
+
+const requestImage= (url)=> new Promise((resolve,reject)=>{
+
+  var requestSettings = {
+    url: url,
+    method: 'GET',
+    encoding: null
+  };
+  
+  request(requestSettings, function(error, response, body) {
+    
+    if(!body)
+      reject("No image file");
+    
+    resolve(body);
+
+  });
+
+});
+
+
+const uploadImageToStorage = (file) => {
+
+  let prom = new Promise((resolve, reject) => {
+    if (!file) {
+      reject('No image file');
+    }
+
+    let fileUpload = bucket.file(file.name);
+
+    const blobStream = fileUpload.createWriteStream({
+      metadata: {
+        contentType: file.mime
+      }
+    });
+
+    blobStream.on('error', (error) => {
+      reject('Something is wrong!');
+    });
+
+    blobStream.on('finish', () => {
+       //The public URL can be used to directly access the file via HTTP.
+
+      const url = util.format(`https://storage.googleapis.com/${bucket.name}/${file.name}`);
+
+      resolve(url);
+
+    });
+
+    blobStream.end(file.buffer);
+
+  });
+  return prom;
+}
+
 
